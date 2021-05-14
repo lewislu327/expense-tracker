e7ace5ced56108f6be61b289b245973a5c4f8e7e
Lewis Lu <agooday32@gmail.com>
2021-05-14 20:13:38 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const port = 3000
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/expense-tracker',{ useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
+const exphbs = require('express-handlebars')
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
 
 db.on('error', () => {
   console.log('mongodb error!')
@@ -16,7 +19,7 @@ db.once('open', () => {
 
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+   res.render('index')
 })
 
 
