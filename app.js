a83bd557ff6460f0be4c2fbd9dd395b49e7dc5ef
Lewis Lu <agooday32@gmail.com>
2021-05-15 17:33:34 +0800

    feat: add create record function

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const exphbs = require('express-handlebars')
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 const Record = require('./models/Record')
+const Category = require('./models/Category')
 
 
 db.on('error', () => {
@@ -17,7 +18,7 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-
+app.use(express.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => {
@@ -27,9 +28,36 @@ app.get('/', (req, res) => {
   .catch(error => console.error(error))
 })
 
+app.get('/new', (req, res) => {
+  return res.render('new')
+})
 
+app.post('/', (req, res) => {
+  const record = req.body
+  const category = req.body.category
+  record.icon = categoryToIcon(category)  
+  //之後要想更好的切換方法
+  return Record.create(record)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 
 app.listen(port, () => {
   console.log(`APP is running on http://localhost:${port}`)
 })
+
+function categoryToIcon(category) {
+  switch (category) {
+    case 'house':
+      return 'fas fa-home';
+    case 'traffic':
+      return 'fas fa-shuttle-van';
+    case 'entertainment':
+      return 'fas fa-grin-beam';
+    case 'food':
+      return 'fas fa-utensils';
+    case 'others':
+      return 'fas fa-pen';
+  }
+}
