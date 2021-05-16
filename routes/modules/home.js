6814edcd8a95bae3bd685343743d4adbce9ee3a4
Lewis Lu <agooday32@gmail.com>
2021-05-16 12:30:54 +0800

    feat: add sum total function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,7 +6,11 @@ router.get('/', (req, res) => {
   Record.find()
   .lean()
   .sort({ date: 'asc' }) 
-  .then( records => res.render('index', {records} ))
+  .then( records => {
+    const total = []
+    records.forEach(data => total.push(Number(data.amount)))
+    (res.render('index', {records, total} ))
+    })
   .catch(error => console.error(error))
 })
 
