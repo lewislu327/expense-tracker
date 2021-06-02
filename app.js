743ff2179e5c53d63bd273841663e87dd8a591da
Lewis Lu <agooday32@gmail.com>
2021-06-02 18:55:48 +0800

    feat: add flash message & bcrypt.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const methodOverride = require('method-override')
 const routes = require('./routes') 
 const session = require('express-session')
 const usePassport = require('./config/passport')
+const flash = require('connect-flash')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs', helpers: multihelpers}))
 app.set('view engine', 'hbs')
@@ -23,9 +24,12 @@ app.use(session({
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 app.use(routes)
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
 const passport = require('passport')
+const bcrypt = require('bcryptjs') 
 
 router.get('/login', (req,res) => {
   res.render('login')
@@ -18,21 +19,41 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
-  User.findOne({ email })
-  .then( user => {
-    if (user){
-      console.log('User already exists!')
-      res.render('register', { name, email, password, confirmPassword })
-    } else {
-      return User.create({ name, email, password })
+  const errors = []
+  if ( !name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+  User.findOne({ email }).then( user => {
+    if ( user ) {
+      errors.push({ message: '這個 Email 已經註冊過了。' })
+      return res.render('register', {
+        errors,name, email, password, confirmPassword
+      })
+    } 
+    return bcrypt
+      .genSalt(10)
+      .then( salt => bcrypt.hash(password, salt))
+      .then( hash => User.create({ name, email, password: hash }))
       .then(() => res.redirect('/'))
-      .catch( err => console.log('err'))
-    }
-  })
+      .catch(err => console.log(err))
+  })  
 })
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
